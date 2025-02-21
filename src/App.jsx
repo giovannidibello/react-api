87e4602964d61aed356ca138e484c85a3666f5fb
Giovanni <dibellogio@gmail.com>
2025-02-21 13:48:06 +0100

    add form input data

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,18 @@ import { useState, useEffect } from 'react'
 import axios from "axios";
 import './App.css'
 
+//  aggiungo i campi vuoti al form
+const initialFormData = {
+  title: "",
+  image: "",
+  content: "",
+  tags: "",
+};
 
 function App() {
 
   const [postsList, setPostsList] = useState([]);
+  const [formData, setFormData] = useState(initialFormData);
 
   // funzione chiamata api tramite axios  
   function fetchTodos() {
@@ -27,9 +35,67 @@ function App() {
     }
   }
 
+  // funzione di gestione delle info dei campi
+  function handleFormData(e) {
+
+    const value = e.target.type === "checkbox" ? e.target.checked : e.target.value;
+    // setto tramite lo stato l'oggetto con le info prese dai campi del form
+    setFormData((currentFormData) => ({
+      ...currentFormData,
+      [e.target.name]: value,
+    }));
+  }
+
+  // funzione di gestione dell'invio del form
+  function handleSubmit(e) {
+    e.preventDefault();
+    setPostsList((currentPost) => [...currentPost, { id: currentPost.length === 0 ? 1 : currentPost[currentPost.length - 1].id + 1, ...formData }]);
+    setFormData(initialFormData);
+  }
+
 
   return (
     <>
+      <form id='formpost' onSubmit={handleSubmit}>
+        {/* valore titolo post */}
+        <input
+          type="text"
+          name="title"
+          onChange={handleFormData}
+          value={formData.title}
+          placeholder='Titolo Post'
+        />
+
+        {/* valore immagine */}
+        <input
+          type="text"
+          name="image"
+          onChange={handleFormData}
+          value={formData.image}
+          placeholder='Immagine Post'
+        />
+
+        {/* valore contenuto */}
+        <input
+          type="text"
+          name="content"
+          onChange={handleFormData}
+          value={formData.content}
+          placeholder='Contenuto Post'
+        />
+
+        {/* valore tags */}
+        <input
+          type="text"
+          name="tags"
+          onChange={handleFormData}
+          value={formData.tags}
+          placeholder='Tags Post (separati da , )'
+        />
+
+        {/* bottone di invio info */}
+        <button>Aggiungi</button>
+      </form>
 
       {postsList.length === 0 ? (
         <h1>Non ci sono posts</h1>
@@ -38,7 +104,6 @@ function App() {
           <div className='postItem' key={post.id}>
             <h2>{post.title}</h2>
             <img src={post.image} alt={post.title} />
-            <h4>{post.autore}</h4>
             <p>{post.content}</p>
             <p>{post.tags.join(", ")}</p>
             <button onClick={() => removePost(post.id)}>Cancella Post</button>
